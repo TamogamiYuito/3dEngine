fdea03e71305c8a95933a99d9e193258e6134a90
Gayu2525 <tamogamiyuito@gmail.com>
2025-06-18 11:49:10 +0900

    a

--- 3dEngine/Main.cpp
+++ 3dEngine/Main.cpp
@@ -199,7 +199,7 @@ void Main()
                         double ang = s3d::Random(-s3d::Math::Pi / 4.0, s3d::Math::Pi / 4.0);
 
                         // Spawn direction (in front of the player)
-                        V3 dir = norm(std::cos(ang) * (-Fh) + std::sin(ang) * Rv);
+                        V3 dir = norm(std::cos(ang) * (-1 * Fh) + std::sin(ang) * Rv);
 
                         // Random distance between 2 and 4 cubes
                         double dist = s3d::Random(2.0, 4.0) * (2.0 * HALF);
