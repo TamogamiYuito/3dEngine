5dbcc87f3e4a2af586bf74caa65ef4463ac0c293
Gayu2525 <tamogamiyuito@gmail.com>
2025-06-24 09:45:23 +0900

    あ

--- 3dEngine/Game.cpp
+++ 3dEngine/Game.cpp
@@ -393,21 +393,7 @@ void Game::run() {
 			
 
             for (const auto& f : FACE) {
-                V3 v0 = toView(vw[f[0]]);
-                V3 v1 = toView(vw[f[1]]);
-                V3 v2 = toView(vw[f[2]]);
-                V3 n = cross(v1 - v0, v2 - v0);
-				if (n.z > EPS) {
-					V3 w0 = vw[f[0]];
-					V3 w1 = vw[f[1]];
-					V3 w2 = vw[f[2]];
-					V3 faceNormal = cross(w1 - w0, w2 - w0);
-					V3 center = (w0 + w1 + w2) / 3.0;
-					V3 toCam = cam - center;
-					constexpr double EPS = 1e-5;
-
-					if (dot(faceNormal, toCam) <= EPS) continue; // 背面
-				}
+                
 
                 if (std::isinf(vp[f[0]].x) || std::isinf(vp[f[1]].x) ||
                     std::isinf(vp[f[2]].x) || std::isinf(vp[f[3]].x)) continue;
