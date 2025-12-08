814ebedb2c521089b6ae4782db4b860a7e5a08be
TamogamiYuito <137864669+TamogamiYuito@users.noreply.github.com>
2025-12-08 15:05:49 +0900

    Refactor game loop structure

--- 3dEngine/Game.hpp
+++ 3dEngine/Game.hpp
@@ -5,6 +5,7 @@
 #include "Light.hpp"
 #include <vector>
 #include <unordered_set>
+#include <optional>
 
 class Game {
 public:
@@ -12,6 +13,18 @@ public:
     void run();
 
 private:
+    struct FrameContext {
+        s3d::Vec2 cursor;
+        s3d::Vec2 windowHalf;
+        V3 cam;
+        V3 right;
+        V3 up;
+        V3 forward;
+
+        P2 project(V3 w) const;
+        std::optional<double> cursorAngle(s3d::Vec2 p, V3 axis, V3 pivot) const;
+    };
+
     std::vector<Cube> cubes;
     std::unordered_set<GKey, GHash> grid;
     Camera camera;
@@ -25,6 +38,13 @@ private:
     Handle activeHd = Handle::None;
     Drag   drag;
 
+    FrameContext buildFrameContext(const s3d::Vec2& windowHalf) const;
+    void updateModeState(bool free);
+    void handleCreationUI(const FrameContext& ctx, double dt, bool free);
+    void updateHoverState(const FrameContext& ctx, bool free);
+    void handleSelectionAndDragStart(const FrameContext& ctx, bool free);
+    void handleDragEnd();
+    void updateDrag(const FrameContext& ctx);
     int detectHoveredCube(const s3d::Vec2& cur, double cx, double cy);
     int detectHoveredLight(const Vec2& cur, double cx, double cy, bool free);
     void handleFPSMovement(double dt);
