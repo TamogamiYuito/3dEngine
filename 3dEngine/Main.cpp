f017b0c5c6655f763c12add7629a02ee322e08a2
Gayu2525 <tamogamiyuito@gmail.com>
2025-12-10 10:53:55 +0900

    Revert "Merge pull request #71 from TamogamiYuito/codex/make-main-function-a-function-dictionary"
    This reverts commit d1062ea2118a9ddd1ad449ed280bde1adcabdb6c, reversing
    changes made to cf840ce60585146fa14c68723ed6e61f7aaf9c81.

--- 3dEngine/Main.cpp
+++ 3dEngine/Main.cpp
@@ -6,7 +6,6 @@
   W / E / R : Move / Rotate / Scale
  -------------------------------------------------*/
 # include <Siv3D.hpp>
-# include <functional>
 # include <vector>
 # include "Math.hpp"
 # include "Cube.hpp"
@@ -15,30 +14,8 @@
 # include "RenderUtils.hpp"
 # include "Game.hpp"
 
-void runGame()
-{
-    Game game;
-    game.run();
-}
-
 void Main()
 {
-    HashTable<String, std::function<void()>> entryPoints = {
-        { U"game", runGame },
-    };
-
-    const Array<String> args = System::GetArguments();
-    const String key = (1 < args.size()) ? args[1] : U"game";
-    if (auto it = entryPoints.find(key); it != entryPoints.end())
-    {
-        it->second();
-    }
-    else
-    {
-        Console << U"Unknown entry: " << key << U"\nAvailable entries:";
-        for (const auto& [name, _] : entryPoints)
-        {
-            Console << U"\n - " << name;
-        }
-    }
+	Game game;
+	game.run();
 }
