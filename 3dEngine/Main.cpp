35461c8dd275972948c22c2d1de9cbb9103d6112
TamogamiYuito <137864669+TamogamiYuito@users.noreply.github.com>
2025-12-10 10:42:34 +0900

    Add entry point dictionary to Main

--- 3dEngine/Main.cpp
+++ 3dEngine/Main.cpp
@@ -6,6 +6,7 @@
   W / E / R : Move / Rotate / Scale
  -------------------------------------------------*/
 # include <Siv3D.hpp>
+# include <functional>
 # include <vector>
 # include "Math.hpp"
 # include "Cube.hpp"
@@ -14,8 +15,30 @@
 # include "RenderUtils.hpp"
 # include "Game.hpp"
 
+void runGame()
+{
+    Game game;
+    game.run();
+}
+
 void Main()
 {
-	Game game;
-	game.run();
+    HashTable<String, std::function<void()>> entryPoints = {
+        { U"game", runGame },
+    };
+
+    const Array<String> args = System::GetArguments();
+    const String key = (1 < args.size()) ? args[1] : U"game";
+    if (auto it = entryPoints.find(key); it != entryPoints.end())
+    {
+        it->second();
+    }
+    else
+    {
+        Console << U"Unknown entry: " << key << U"\nAvailable entries:";
+        for (const auto& [name, _] : entryPoints)
+        {
+            Console << U"\n - " << name;
+        }
+    }
 }
